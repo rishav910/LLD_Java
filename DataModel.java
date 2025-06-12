822ecc2c58c75ce272e66d0d1aa2889b9f0e736c
Rishav Saha <78402961+rishav910@users.noreply.github.com>
2025-06-12 15:27:56 +0530

    MongoDB data model

---
+++ DataModel.java
@@ -0,0 +1,24 @@
+package com.rishav.store;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document (collection = "name")
+public class DataModel {
+    @Id
+    private String id;
+
+    @Field("name")
+    private String name;
+
+    public DataModel(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    // Setters and getters
+    public String getID () {
+        return this.id;
+    }
+}
